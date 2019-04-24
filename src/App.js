4881c311c3ed6fe4b1ac9a687d8c7989b78e650f
Eric Mauldin <emauldin84@gmail.com>
2019-04-24 10:15:17 -0400

    initial commit with form to print input to screen

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Output from './Output';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputText: ''
+    }
+  }
+  
+  render () {
+    console.log('Rendering App')
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input onChange={ (e) => {
+              console.log(e.target.value);
+              this.setState({
+                inputText: e.target.value
+              })
+            }}/>
+            
+  
+  
+          <Output
+            text={this.state.inputText}
+            />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Output.js
@@ -0,0 +1,23 @@
+// V1. The purpose of this component is to receive some text and display it to the page
+
+// V2. Add "translation" (toUpperCase) to the text displayed to the page
+
+import React from 'react';
+
+function Output(props) {
+    console.log('Rendering Output')
+    return (
+        <div>{props.text.toUpperCase()}</div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+export default Output;
